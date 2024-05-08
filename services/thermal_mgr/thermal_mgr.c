6e7578829d4379e9796a39af943dbac5d8e7e119
Ameen Durani <uni.ameend@gmail.com>
2024-05-08 15:50:01 -0400

    Updated code for level 3

--- services/thermal_mgr/thermal_mgr.c
+++ services/thermal_mgr/thermal_mgr.c
@@ -43,30 +43,36 @@ void initThermalSystemManager(lm75bd_config_t *config) {
 
 error_code_t thermalMgrSendEvent(thermal_mgr_event_t *event) {
   /* Send an event to the thermal manager queue */
-  xQueueSend(thermalMgrQueueHandle, event, 1);
+  xQueueSend(thermalMgrTaskHandle, event, 1);
 
   return ERR_CODE_SUCCESS;
 }
 
 void osHandlerLM75BD(void) {
   /* Implement this function */
-  float *temp;
-  readTempLM75BD(LM75BD_OBC_I2C_ADDR, temp);
-  if(*temp >= 80){overTemperatureDetected();}
-  if(*temp <= 75){safeOperatingConditions();}
+  thermal_mgr_event_t interup;
+  interup.type = InteruptSqn;
+  thermalMgrSendEvent(&interup);
 
 }
 
 static void thermalMgr(void *pvParameters) {
   /* Implement this task */
-  float *temp;
+
+  float temp;
+  thermal_mgr_event_t buff;
 
   while (1) {
+   xQueueReceive(thermalMgrQueueHandle, &buff, 1);
 
-   xQueueReceive(thermalMgrQueueHandle, pvParameters, 1);
-   if (pvParameters == THERMAL_MGR_EVENT_MEASURE_TEMP_CMD){
-      readTempLM75BD(LM75BD_OBC_I2C_ADDR, temp);
-      addTemperatureTelemetry(*temp);
+   if (buff.type == THERMAL_MGR_EVENT_MEASURE_TEMP_CMD){
+      readTempLM75BD(LM75BD_OBC_I2C_ADDR, &temp);
+      addTemperatureTelemetry(temp);
+   }
+
+   else if( buff.type == InteruptSqn){
+      readTempLM75BD(LM75BD_OBC_I2C_ADDR, &temp);
+      (temp>75) ? overTemperatureDetected(): safeOperatingConditions();
    }
 
   }
